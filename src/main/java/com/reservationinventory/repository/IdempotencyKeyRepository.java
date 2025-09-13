b4ac17defcd1affb9eb56c710bacb5466cb15999
adityamehta242 <adityamehta242@gmail.com>
2025-09-13 11:09:48 +0530

    add sample-application.properties

--- src/main/java/com/reservationinventory/repository/IdempotencyKeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.reservationinventory.repository;
-
-import com.reservationinventory.entity.IdempotencyKey;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IdempotencyKeyRepository extends JpaRepository<IdempotencyKey, String> {
-}
